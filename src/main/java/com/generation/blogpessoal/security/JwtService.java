ae1f4ae702d8b6f14424f9ed3387a704d246bb61
Milena <milenasvitras@gmail.com>
2025-10-20 17:11:35 -0300

    :sparkles: Modificação no arquivo JwtService

--- src/main/java/com/generation/blogpessoal/security/JwtService.java
+++ src/main/java/com/generation/blogpessoal/security/JwtService.java
@@ -14,7 +14,7 @@ import io.jsonwebtoken.security.Keys;
 @Component
 public class JwtService {
  
-    private static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+    private static final String SECRET = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
     private static final Duration EXPIRATION_DURATION = Duration.ofMinutes(60);
     private final SecretKey signingKey;
 
